bd72b7f557cf05d9963e4ac729b770b491b1ef6e
Sapro28 <saherz123@outlook.com>
2025-08-23 22:17:12 +0300

    setup authentication using puter API

--- app/routes/Auth.tsx
+++ app/routes/Auth.tsx
@@ -1,4 +1,7 @@
 import * as domain from "node:domain";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
     { title: 'Resumeh | Auth' },
@@ -6,6 +9,15 @@ export const meta = () => ([
 ])
 
 const Auth = () => {
+    const { isLoading , auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }), [auth.isAuthenticated, next]
+
   return (
       <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
           <div className="gradient-border shadow-lg">
@@ -13,6 +25,22 @@ const Auth = () => {
                   <div className="flex flex-col items-center gap-2 text-center">
                       <h1>Welcome</h1>
                       <h2>Log In to continue your job journey</h2>
+                  </div>
+                  <div>
+                      {isLoading ? (
+                          <button className="auth-button animate-pulse">
+                              <p>Signing you in...</p>
+                          </button>
+                      ) : (
+                          <>
+                              {auth.isAuthenticated ? (
+                                  <button className="auth-button" onClick={auth.signOut}><p>Log Out</p></button>
+                              ) : (
+                                  <button className="auth-button" onClick={auth.signIn}><p>Log In</p></button>
+                              )
+                              }
+                          </>
+                      )}
                   </div>
               </section>
           </div>
